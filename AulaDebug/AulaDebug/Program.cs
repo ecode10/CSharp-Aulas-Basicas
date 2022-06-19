f9def50d39b802c0e0b80034abadd1ccb35be6df
Mauricio Junior <mauricio.junior@gmail.com>
2022-06-19 08:47:36 -0500

    debug
    aula de debug
    criando dois métodos
    veja no https://ecode10.academy

---
+++ AulaDebug/AulaDebug/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AulaDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Escrever();
+            Calcular();
+        }
+
+        static void Escrever()
+        {
+            Console.WriteLine("Consegui escrever um método");
+        }
+
+        static void Calcular()
+        {
+            var total = 0;
+            while (total < 10)
+            {
+                Console.WriteLine($"O Total agora é: {total}");
+                total++;
+            }
+        }
+    }
+}
